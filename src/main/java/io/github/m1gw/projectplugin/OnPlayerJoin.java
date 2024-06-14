db667c84e459010b051fb2086a2e53fd10269bb4
Mohamad Abdulwahid <mohamd.abdulwahid@outlook.com>
2024-06-14 10:46:28 +0200

    no

---
+++ src/main/java/io/github/m1gw/projectplugin/OnPlayerJoin.java
@@ -0,0 +1,7 @@
+package io.github.m1gw.projectplugin;
+
+import org.bukkit.event.Listener;
+
+public class OnPlayerJoin implements Listener {
+
+}
--- src/main/java/io/github/m1gw/projectplugin/ProjectPlugin.java
+++ src/main/java/io/github/m1gw/projectplugin/ProjectPlugin.java
@@ -1,5 +1,6 @@
 package io.github.m1gw.projectplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ProjectPlugin extends JavaPlugin {
@@ -7,7 +8,7 @@ public final class ProjectPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(new OnPlayerJoin(), this);
     }
 
     @Override
